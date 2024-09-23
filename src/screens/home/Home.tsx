d4adc6052903121ea228d52ff533d2a0291b09cc
ht-dv <ht.thedev@gmail.com>
2024-09-24 00:54:04 +0300

    add image to home

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,16 +1,32 @@
+'use client'
 import {Images} from '@/theme'
 import Image from 'next/image'
 import styles from './Home.module.css'
+import {useRouter} from 'next/navigation'
 
 const Home = () => {
+  const router = useRouter()
   return (
-    // <div style={{minHeight: '100vh'}}>
-    <Image
-      src={Images.home.yatirimTesvik}
-      alt="Yatırım Teşvik Görsel"
-      className={styles.images}
-    />
-    // </div>
+    <div
+    // style={{minHeight: '100vh'}}
+    >
+      <div className={styles.textContainer}>
+        <h1>Yatırım Teşvik Belgesi</h1>
+        <p>
+          Yatırımlarınızı gerçekleştirmeden önce alacağınız belge sayesinde KDV
+          istisnası, gümrük vergisi muafiyeti, vergi indirimi gibi birçok
+          avantajdan yararlanabilirsiniz.
+        </p>
+        <button onClick={() => router.push('/hizmetlerimiz/yatirim-tesvik')}>
+          Detaylı Bilgi İçin Tıklayınız
+        </button>
+      </div>
+      <Image
+        src={Images.home.yatirimTesvik}
+        alt="Yatırım Teşvik Görsel"
+        className={styles.images}
+      />
+    </div>
   )
 }
 export default Home
