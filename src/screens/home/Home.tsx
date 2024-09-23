60f005b9b022964b4502ea240751ccccba867b38
ht-dv <ht.thedev@gmail.com>
2024-09-24 00:07:24 +0300

    update image

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -4,13 +4,13 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div style={{minHeight: '100vh'}}>
-      <Image
-        src={Images.home.yatirimTesvik}
-        alt="Yatırım Teşvik Görsel"
-        className={styles.images}
-      />
-    </div>
+    // <div style={{minHeight: '100vh'}}>
+    <Image
+      src={Images.home.yatirimTesvik}
+      alt="Yatırım Teşvik Görsel"
+      className={styles.images}
+    />
+    // </div>
   )
 }
 export default Home
